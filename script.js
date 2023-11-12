ff004cc8dec0e373d7119e35afd247c04d73a6a1
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-11-12 13:55:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const counter = document.querySelector("#counter");
+const btn = document.querySelector("#incrementBtn");
+
+let count= 0;
+btn.addEventListener("click",function(){
+
+  counter.value = count;
+  alert(count)
+  count++
+  counter.innerText = count
+  
+
+})
